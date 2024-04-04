2ca9a6083e3a22c29e7de284523685b206c5f5c8
nedoratic <nedoratic@gmail.com>
2024-04-04 21:43:17 +0200

    index.js - is spam function added

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
 const messageInput = document.getElementById('message-input');
 const result = document.getElementById('result');
 const checkMessageButton = document.getElementById('check-message-btn');
+
+// Is Spam Function
+const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
