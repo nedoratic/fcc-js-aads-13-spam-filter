f7604a0a9c44ecf403087207c3f2b588733774ef
nedoratic <nedoratic@gmail.com>
2024-04-05 20:02:08 +0200

    index.js - button on click even listener added

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ const checkMessageButton = document.getElementById('check-message-btn');
 
 // Is Spam Function
 const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
+
+// Event Listener
+checkMessageButton.addEventListener('click', () => {
+	if (messageInput.value === '') {
+		alert('Please enter a message.');
+		return;
+	}
+	result.textContent = isSpam(messageInput.value) ? 'Oh no! This looks like a spam message.' : 'This message does not seem to contain any spam.';
+	messageInput.value = '';
+});
